895cc580fa1a2adcd5a2a35791dd875dc7c891b9
Loris Guignard <loris.guignard@gmail.com>
2016-04-10 19:23:26 +0200

    Update tests (and move process.env init in fixtures file)

---
+++ test/_fixtures.js
@@ -0,0 +1,8 @@
+process.env.HEROKU_APP_ID = '2d9a4aaf-1023-4520-8ed1-d6fda6fd86d6';
+process.env.HEROKU_APP_NAME = 'heroku-dyno-metadata';
+process.env.HEROKU_DYNO_ID = 'd1576980-3f3e-4593-b7b9-6c2fbf874e07';
+process.env.DYNO = 'web.1';
+process.env.HEROKU_SLUG_COMMIT = 'cbdae5a0cd1f5a0f0a67b3d82844ceb33d0caed7';
+process.env.HEROKU_SLUG_DESCRIPTION = 'Deploy cbdae5a';
+process.env.HEROKU_RELEASE_CREATED_AT = '2016-04-08T06:39:53Z';
+process.env.HEROKU_RELEASE_VERSION = 'v832';
--- test/index.js
+++ test/index.js
@@ -3,27 +3,27 @@ import _ from 'lodash';
 import express from 'express';
 import request from 'supertest-as-promised';
 
-import herokuDynoMetadata, { mapping } from '../src';
-
-process.env.HEROKU_APP_ID = '2d9a4aaf-1023-4520-8ed1-d6fda6fd86d6';
-process.env.HEROKU_APP_NAME = 'heroku-dyno-metadata';
-process.env.HEROKU_DYNO_ID = 'd1576980-3f3e-4593-b7b9-6c2fbf874e07';
-process.env.DYNO = 'web.1';
-process.env.HEROKU_SLUG_COMMIT = 'cbdae5a0cd1f5a0f0a67b3d82844ceb33d0caed7';
-process.env.HEROKU_SLUG_DESCRIPTION = 'Deploy cbdae5a';
-process.env.HEROKU_RELEASE_CREATED_AT = '2016-04-08T06:39:53Z';
-process.env.HEROKU_RELEASE_VERSION = 'v832';
+import metadata, { mapping, middleware } from '../src';
 
 function expressApp(keys) {
-  const app = express();
+  const app = express()
+    .set('port', 0);
 
-  app.use(herokuDynoMetadata(keys));
+  app.use(middleware(keys));
   app.get('/', (req, res) => res.send());
 
   return app;
 }
 
-test('herokuDynoMetadata sets headers', async t => {
+test('default export contains metadata object', t => {
+  t.plan(_.size(mapping));
+
+  _.forEach(mapping, (value, key) => {
+    t.is(metadata[key], process.env[value]);
+  });
+});
+
+test('middleware sets headers', async t => {
   t.plan(_.size(mapping) + 1);
 
   const res = await request(expressApp())
@@ -31,19 +31,20 @@ test('herokuDynoMetadata sets headers', async t => {
 
   t.is(res.status, 200);
   _.forEach(mapping, (value, key) => {
-    t.is(res.get(key), process.env[value]);
+    const header = `x-heroku-${_.kebabCase(key)}`;
+    t.is(res.get(header), process.env[value]);
   });
 });
 
-test('herokuDynoMetadata sets selected headers', async t => {
-  t.plan(_.size(mapping) + 1);
+test('middleware sets picked headers only', async t => {
+  t.plan(_.size(mapping));
 
   const keys = ['x-heroku-app-id', 'x-heroku-dyno-id', 'x-heroku-release-version'];
   const res = await request(expressApp(keys))
     .get('/');
 
-  t.is(res.status, 200);
   _.forEach(mapping, (value, key) => {
-    t.is(res.get(key), _.includes(keys, key) ? process.env[value] : undefined);
+    const header = `x-heroku-${_.kebabCase(key)}`;
+    t.is(res.get(header), _.includes(keys, header) ? process.env[value] : undefined);
   });
 });
