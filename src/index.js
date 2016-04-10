be932684dcf6be8555bd4663b79c784c7e1c6528
Loris Guignard <loris.guignard@gmail.com>
2016-04-10 19:22:50 +0200

    Export headers object (close #3)

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
 import _ from 'lodash';
 
+const headerPrefix = 'x-heroku';
+
 export const mapping = {
-  'x-heroku-app-id': 'HEROKU_APP_ID',
-  'x-heroku-app-name': 'HEROKU_APP_NAME',
-  'x-heroku-dyno-id': 'HEROKU_DYNO_ID',
-  'x-heroku-dyno-name': 'DYNO',
-  'x-heroku-slug-commit': 'HEROKU_SLUG_COMMIT',
-  'x-heroku-slug-description': 'HEROKU_SLUG_DESCRIPTION',
-  'x-heroku-release-created-at': 'HEROKU_RELEASE_CREATED_AT',
-  'x-heroku-release-version': 'HEROKU_RELEASE_VERSION',
+  appId: 'HEROKU_APP_ID',
+  appName: 'HEROKU_APP_NAME',
+  dynoId: 'HEROKU_DYNO_ID',
+  dynoName: 'DYNO',
+  slugCommit: 'HEROKU_SLUG_COMMIT',
+  slugDescription: 'HEROKU_SLUG_DESCRIPTION',
+  releaseCreatedAt: 'HEROKU_RELEASE_CREATED_AT',
+  releaseVersion: 'HEROKU_RELEASE_VERSION',
 };
 
+const metadata = _.mapValues(mapping, value => process.env[value]);
+
 function getHeaders(keys = _.keys(mapping)) {
-  return _(mapping)
+  return _(metadata)
+    .mapKeys((val, key) => `${headerPrefix}-${_.kebabCase(key)}`)
     .pick(keys)
-    .mapValues(value => process.env[value])
     .omitBy(_.isUndefined)
     .value();
 }
 
-export default function (keys) {
-  return function expressMiddleware(req, res, next) {
+export function middleware(keys) {
+  return (req, res, next) => {
     const headers = getHeaders(keys);
-    res.set(headers);
     if (headers) {
+      res.set(headers);
     }
 
     next();
   };
 }
+
+export default metadata;
